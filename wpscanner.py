d18c1365fffea46add4115b76f2e6acbccdc79a2
Sankalpa baral <68674370+SANKALPA-web@users.noreply.github.com>
2021-08-06 11:58:30 +0545

    Update wpscanner.py

--- wpscanner.py
+++ wpscanner.py
@@ -1,71 +1,80 @@
 # Wordpress vulnerability scanner
-# --SankalpaBaral--
+# --Made--by--SankalpaBaral--
 
 import requests
+import pyfiglet
+text = pyfiglet.figlet_format("WP SCANNER")
+print(text)
+
+
 print("Enter the url without adding https/http\n")
 
 url = input("Enter the target url ")
 target = ("https://"+url)
 
-print("Scanning",target)
-print("")
-
+print("Scanning",target+"\n")
 
 
 a = requests.get("https://"+url+"/xmlrpc.php")
 if a.status_code==405:
-    print("[!]xmlrpc.php file is enabled\n")
+    print("[!] xmlrpc.php file is enabled\n")
 else:
-    print("[!]xmlrpc.php file is disabled\n")
+    print("[!] xmlrpc.php file is disabled\n")
 
 
 b = requests.get("https://"+url+"/wp-json/")
 if b.status_code==200:
-    print("[!]wp-json file is enabled\n")
+    print("[!] wp-json file is enabled\n")
 else:
-    print("[!]wp-json file is disabled\n")
+    print("[!] wp-json file is disabled\n")
 
 c = requests.get("https://"+url+"/admin")
 if c.status_code==200:
-    print("[!]Admin pannel is enabled\n")
+    print("[!] Admin pannel is enabled\n")
 else:
-    print("[!]Admin pannel is disabled\n")
+    print("[!] Admin pannel is disabled\n")
 
 d = requests.get("https://"+url+"/robots.txt")
 if d.status_code==200:
-    print("[!]Robots.txt file is enabled\n")
+    print("[!] Robots.txt file is enabled\n")
 else:
-    print("[!]Robots.txt is disabled\n")
+    print("[!] Robots.txt is disabled\n")
 
 e = requests.get("https://"+url+"/wp-content/uploads")
 if e.status_code==200:
-    print("[!]Direcotry travelsal  is enabled\n")
+    print("[!] Direcotry travelsal  is enabled\n")
 else:
     print("[!]Direcotry travelsal is disabled\n")
 
 e = requests.get("https://"+url+"/sitemap.xml")
 if e.status_code==200:
-    print("[!]Sitemap.xml file is enabled\n")
+    print("[!] Sitemap.xml file is enabled\n")
 else:
-    print("[!]Sitemap.xml file is disabled\n")
+    print("[!] Sitemap.xml file is disabled\n")
 
 f = requests.get("https://"+url+"/.htaccess")
 if f.status_code==200:
-    print("[!].htaccess file is enabled\n")
+    print("[!] .htaccess file is enabled\n")
 else:
-    print("[!].htaccess file is disabled\n")
+    print("[!] .htaccess file is disabled\n")
 
 g = requests.get("https://"+url+"/.gitignore")
 if g.status_code==200:
-    print("[!].gitignore file is enabled\n")
+    print("[!] .gitignore file is enabled\n")
 else:
-    print("[!].gitignore file is disabled\n")
+    print("[!] .gitignore file is disabled\n")
 
 h = requests.get("https://"+url+"/.log")
 if h.status_code==200:
-    print("[!].log file is enabled\n")
+    print("[!] .log file is enabled\n")
 else:
-    print("[!].log file is disabled\n")
+    print("[!] .log file is disabled\n")
+
+i = requests.get(target+"/license.txt")
+if i.status_code==200:
+    print("[!] license.txt file is enabled\n")
+else:
+    print("[!] license.txt file is disabled\n")
 
 
      
